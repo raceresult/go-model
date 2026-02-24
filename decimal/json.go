24f2ef4ce3d4dc28d6efe848b20400d503cbed4b
Benedict Wetzel <bewe@raceresult.com>
2026-02-24 16:55:11 +0100

    fix(decimal): add comment

--- decimal/json.go
+++ decimal/json.go
@@ -9,6 +9,7 @@ func (s Decimal) MarshalJSON() ([]byte, error) {
 
 // UnmarshalJSON parses a Decimal from JSON
 func (s *Decimal) UnmarshalJSON(data []byte) error {
+	// UnmarshalJSON is called with quotes in the byte slice when unmarshalling a map key
 	trimmed := strings.Trim(string(data), "\"")
 	x, err := FromString(trimmed)
 	if err != nil {

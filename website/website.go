2a3689d93a824763cf84e8189bbceaa0403978d1
Soeren Lindner <lindner@raceresult.com>
2026-09-02 13:28:04 +0200

    feat(website): rename HideProfilePicture field to HideProfilePictures in ConfigList

--- website/website.go
+++ website/website.go
@@ -119,17 +119,17 @@ type TabListsConfig struct {
 }
 
 type ConfigList struct {
-	Name               string
-	Mode               string
-	Contest            interface{}
-	ShowAs             string
-	Format             string
-	Live               interface{} `json:",omitempty"`
-	Sortable           interface{} `json:",omitempty"`
-	Leader             interface{} `json:",omitempty"`
-	Details            string      `json:",omitempty"`
-	ID                 string      `json:",omitempty"`
-	HideProfilePicture bool        `json:",omitempty"`
+	Name                string
+	Mode                string
+	Contest             interface{}
+	ShowAs              string
+	Format              string
+	Live                interface{} `json:",omitempty"`
+	Sortable            interface{} `json:",omitempty"`
+	Leader              interface{} `json:",omitempty"`
+	Details             string      `json:",omitempty"`
+	ID                  string      `json:",omitempty"`
+	HideProfilePictures bool        `json:",omitempty"`
 }
 
 // Details Tab
